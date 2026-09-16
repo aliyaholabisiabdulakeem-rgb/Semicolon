6668fe36936cce1203978c5e0b99e4a0d625b1cf
Aliyah <aliyaholabisiabdulakeem@gmail.com>
2026-09-17 00:20:40 +0100

    Add files via upload

---
+++ cases.py
@@ -0,0 +1,8 @@
+statement = input("Enter a statement: ")
+
+first = statement[0]
+if first >= 'A' and first <= 'Z':
+  print("uppercase")
+
+elif first >= 'a' and first <= 'z':
+  print("lowercase")
