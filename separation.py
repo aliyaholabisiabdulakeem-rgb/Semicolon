a38d88a5588eca40d81eeea0819d13de45fe3718
Aliyah <aliyaholabisiabdulakeem@gmail.com>
2026-09-17 00:35:29 +0100

    Add files via upload

---
+++ separation.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a five digit integer: "))
+
+digit1 = number // 10000
+digit2 = (number // 1000) % 10
+digit3 = (number // 100) % 10
+digit4 = (number // 10) % 10
+digit5 = number % 10
+
+print(digit1, " ", digit2, " ", digit3, " ", digit4, " ", digit5)
