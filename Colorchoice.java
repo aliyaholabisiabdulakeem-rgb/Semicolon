e8859c0cb025fab014a92b5633eb53c67a3e745e
Aliyah <aliyaholabisiabdulakeem@gmail.com>
2026-09-17 00:05:49 +0100

    Add files via upload

---
+++ Colorchoice.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+	public class Colorchoice{
+	public static void main(String[] args){
+ 	Scanner input = new Scanner(System.in);
+System.out.print("Enter an color ");
+ 	String color = input next();
+if color
