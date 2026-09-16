07f72ab62955014924237c2bec65ee5de39bc62b
Aliyah <aliyaholabisiabdulakeem@gmail.com>
2026-09-16 18:22:07 +0100

    Add files via upload

---
+++ Asteroids.java
@@ -0,0 +1,22 @@
+public class Asteroids{
+public static void main(String[] args){
+
+System.out.println("* * * * * * * * ");
+System.out.println(" * * * * * * * *");
+System.out.println("* * * * * * * * ");
+System.out.println(" * * * * * * * *");
+System.out.println("* * * * * * * * ");
+System.out.println(" * * * * * * * *");
+System.out.println("* * * * * * * * ");
+System.out.println(" * * * * * * * *");
+System.out.println("* * * * * * * * ");
+System.out.println(" * * * * * * * *");
+
+
+
+
+
+
+
+}
+}
---
+++ Character.java
@@ -0,0 +1,25 @@
+public class Character{
+public static void main(String[] args){
+
+System.out.printf("The character %c has the value %f%n,'A', ((int)'A'));
+System.out.printf("The character %c has the value %f%n,'B', ((int)'B'));
+System.out.printf("The character %c has the value %f%n,'C', ((int)'C'));
+System.out.printf("The character %c has the value %f%n,'a', ((int)'a'));
+System.out.printf("The character %c has the value %f%n,'b', ((int)'b'));
+System.out.printf("The character %c has the value %f%n,'c', ((int)'c'));
+System.out.printf("The character %c has the value %f%n,'0', ((int)'0'));
+System.out.printf("The character %c has the value %f%n,'1', ((int)'1'));
+System.out.printf("The character %c has the value %f%n,'2', ((int)'2'));
+System.out.printf("The character %c has the value %f%n,'$', ((int)'$'));
+System.out.printf("The character %c has the value %f%n,'*', ((int)'*'));
+System.out.printf("The character %c has the value %f%n,'*', ((int)'*'));
+System.out.printf("The character %c has the value %f%n,'*', ((int)'*'));
+System.out.printf("The character %c has the value %f%n,'/', ((int)'/'));
+System.out.printf("The character %c has the value %f%n,' ', ((int)' '));
+
+
+
+
+}
+
+}
